68fd31463d111feb4deba1ec2b4d11c35f52641c
vMario-Aviatrix <102957943+mjasinski007@users.noreply.github.com>
2023-01-15 17:07:20 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -16,9 +16,8 @@ provider "aws" {
 }
 
 provider "vsphere" {
-    user                 = "administrator@192.168.0.154"
-    password             = "Qaz!741852"
-    #vsphere_server      = "192.168.0.154"
-    vsphere_server       = "89.69.144.149" # Used exernally e.g Terraform Cloud
+    user                 = "administrator@vdevnetpro.com"
+    password             = "CHANGEME"
+    vsphere_server       = "vcenter7srv01.vdevnetpro.com"
     allow_unverified_ssl = true # If you have a self-signed cert
 }
